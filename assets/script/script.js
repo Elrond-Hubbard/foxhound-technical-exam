bf80b5ddbb327e337c442a774164012f61fb1e3d
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-22 19:17:51 -0400

    built object array for questions/answers

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,32 @@
 
-// Define each answer button on the page
-const buttonA = document.querySelector("#buttonA");
-const buttonB = document.querySelector("#buttonB");
-const buttonC = document.querySelector("#buttonC");
-const buttonD = document.querySelector("#buttonD");
+// // Define each answer button on the page
+// const buttonA = document.querySelector("#buttonA");
+// const buttonB = document.querySelector("#buttonB");
+// const buttonC = document.querySelector("#buttonC");
+// const buttonD = document.querySelector("#buttonD");
 
-function click() {
-    console.log("CLICK!!!")
-}
+// buttonA.addEventListener("click", click);
+// buttonB.addEventListener("click", click);
+// buttonC.addEventListener("click", click);
+// buttonD.addEventListener("click", click);
+// function click() {
+//     console.log("CLICK!!!")
+// }
 
-buttonA.addEventListener("click", click);
-buttonB.addEventListener("click", click);
-buttonC.addEventListener("click", click);
-buttonD.addEventListener("click", click);
+// Define question and answer elements
+const questionElement = document.getElementById("question");
+const answersElement = document.getElementById("answers")
+
+// Define each question object in an array
+const qArray = [
+    {
+        question: "What is your name?",
+        answers: [
+            {text: "Doug", correct: false},
+            {text: "Jane", correct: false},
+            {text: "Michael", correct: true},
+            {text: "Snake", correct: false},
+        ]
+    }
+];
+
