05579e08d4952e8e509bcb21fe948d0160b4883f
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-22 17:33:12 -0400

    wrote button test script

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+
+// Define each answer button on the page
+const buttonA = document.querySelector("#buttonA");
+const buttonB = document.querySelector("#buttonB");
+const buttonC = document.querySelector("#buttonC");
+const buttonD = document.querySelector("#buttonD");
+
+function click() {
+    console.log("CLICK!!!")
+}
+
+buttonA.addEventListener("click", click);
+buttonB.addEventListener("click", click);
+buttonC.addEventListener("click", click);
+buttonD.addEventListener("click", click);
