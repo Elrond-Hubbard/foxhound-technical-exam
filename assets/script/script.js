a40540b68c46147357f16ae472faa162a259bafe
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-07-25 06:48:11 -0400

    refactored script for readability

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,13 +3,13 @@
 ///// QUIZ GAME /////
 
 
-// HTML element assignments
+// Global variable assignments
 const questionElement = document.getElementById("question");
 const answersElement = document.getElementById("answers");
 const scoreElement = document.getElementById("score");
 const timerElement = document.getElementById("timer")
-startButton = document.getElementById("begin");
-startButton.addEventListener("click", startQuiz);
+const startButton = document.getElementById("begin");
+
 var questionIndex = 0;
 var score = 0;
 var timeRemaining = 30;
@@ -19,101 +19,108 @@ const questionArray = [
     {
         question: "What is your name?",
         answers: [
-            {text: "Doug", correct: false},
-            {text: "Jane", correct: false},
-            {text: "Sir Lancelot of Camelot", correct: true},
-            {text: "Snake", correct: false},
+            { text: "Doug", correct: false },
+            { text: "Jane", correct: false },
+            { text: "Sir Lancelot of Camelot", correct: true },
+            { text: "Snake", correct: false },
         ]
     },
     {
         question: "What is your quest?",
         answers: [
-            {text: "To start a band", correct: false},
-            {text: "To steal one billion dollars", correct: false},
-            {text: "To seek the Holy Grail", correct: true},
-            {text: "To rescue the DARPA Chief", correct: false},
+            { text: "To start a band", correct: false },
+            { text: "To steal one billion dollars", correct: false },
+            { text: "To seek the Holy Grail", correct: true },
+            { text: "To rescue the DARPA Chief", correct: false },
         ]
     },
     {
         question: "What is your favorite color?",
         answers: [
-            {text: "Red", correct: false},
-            {text: "Green", correct: false},
-            {text: "Blue", correct: true},
-            {text: "Yellow", correct: false},
+            { text: "Red", correct: false },
+            { text: "Green", correct: false },
+            { text: "Blue", correct: true },
+            { text: "Yellow", correct: false },
         ]
     }
 ];
 
-    
-// Timer
-function countdown() {
-    var timer = setInterval(function() {
-        timeRemaining--;
-        timerElement.innerText = `TIME: ${timeRemaining}`;
-        if (timeRemaining < 0) {
-            gameOver();
-            clearInterval(timeRemaining);
-            timerElement.innerText = "TIME: 0"
-        }
-        else {}
-    }, 1000);
-}
 
-
-// start quiz
+// When "Begin" is clicked, initialize a countdown and
+// begin the quiz.
+startButton.addEventListener("click", startQuiz);
 function startQuiz() {
     countdown();
     showQuestion();
-}
-
-function gameOver() {
-    questionElement.innerText = "GAME OVER"
-    answersElement.innerHTML = `<h2>submit your score</h2>`;
 }
 
 
 // This function populates the page with values from
 // the currently selected question index.
 function showQuestion() {
+        
+        // Display scorecard
+        scoreElement.innerText = `SCORE: ${score}`;
 
-    // Display timer and scorecard
-    scoreElement.innerText = `SCORE: ${score}`;
+        // Reset quiz containers to prevent stacking
+        answersElement.innerHTML = "";
+        questionElement.innerText = "";
 
-    // Reset quiz containers to prevent stacking
-    answersElement.innerHTML = "";
-    questionElement.innerText = "";
+        // Set the current question to question index value
+        // and push question text to question element
+        currentQuestion = questionArray[questionIndex];
+        questionElement.innerText = currentQuestion.question;
 
-    // Set the current question to question index value
-    // and push question text to question element
-    currentQuestion = questionArray[questionIndex];
-    questionElement.innerText = currentQuestion.question;
-
-    // For each answer, create a button,
-    // define class, and push answer text to it.
-    currentQuestion.answers.forEach(answers => {
-        const button = document.createElement("button");
-        if (answers.correct) {
-            button.classList = "correct"}
-        else {button.classList = "false"};
-        answersElement.appendChild(button);
-        button.innerText = answers.text;
-        button.addEventListener("click", checkAnswer)
-    })
+        // For each answer, create a button,
+        // define class, and push answer text to it.
+        currentQuestion.answers.forEach(answers => {
+            const button = document.createElement("button");
+            if (answers.correct) {
+                button.classList = "correct"
+            }
+            else { button.classList = "false" };
+            answersElement.appendChild(button);
+            button.innerText = answers.text;
+            button.addEventListener("click", checkAnswer)
+        })
 };
 
 
 // A class comparison checks the status of the selected
-// answer. A point is awarded if the answer is correct, and
-// time is deducted if the answer is incorrect.
+// answer and shows the next question.
 function checkAnswer(button) {
     const selectedButton = button.target;
+    // Points are awarded for correct answers.
     if (selectedButton.classList.contains("correct")) {
         score += 100;
         questionIndex += 1;
-    } else {
-        timeRemaining -= 20
+    // Time is deducted for incorrect answers
+    } else if (selectedButton.classList.contains("false")) {
+        timeRemaining -= 20;
         questionIndex += 1;
     }
-    showQuestion()
+    showQuestion();
+}
+
+
+
+// When timer runs out, a game over screen is displayed.
+function countdown() {
+    var timer = setInterval(function () {
+        timeRemaining--;
+        timerElement.innerText = `TIME: ${timeRemaining}`;
+        if (timeRemaining < 0 || questionIndex > questionArray.length - 1) {
+            clearInterval(timer);
+            gameOver();
+        }
+    }, 1000);
+}
+
+// The game over screen prompts the user for initials and displays
+// recent scores.
+function gameOver() {
+    questionElement.innerText = "GAME OVER";
+    answersElement.innerHTML = `<h2>submit your score</h2>`;
+    timerElement.innerText = "TIME OUT!";
+    clearInterval(timer);
 }
